4a9734edc01bb15ad7cb714a9f58698ec0e0bdc6
R Brown <rabwent11 at gmail dot com>
2020-08-20 01:29:07 +0100

    for each building check if it refers to an uploaded profile file or if a generic one must be made

--- api/handle_base_loads.py
+++ api/handle_base_loads.py
@@ -1,5 +1,15 @@
 
 
 def list_buildings(query):
-	print(type(query))
-	print('number of buildings: ',  len(query['building_data']) )
+    print(type(query))
+    print('number of buildings: ',  len(query['building_data']) )
+    for building in query['building_data']:
+        # print('\n', building, end='\n\n')
+        if building['load_profile_csv_optional']:
+            print( f"building: {building['name']} refers to a load file:",
+                    f" {building['load_profile_csv_optional']} ")
+        else:
+            print( f"building: {building['name']} needs generic load:",
+                    f" profile type: {building['building_type']} ",
+                    f" annual consumption in kWh: "
+                    f"{building['annual_kwh_consumption_optional']}")
