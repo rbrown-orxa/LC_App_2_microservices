7cf6ed1115104cf351f0714759a18fded7861abd
Robert <rabwent11@gmail.com>
2020-09-17 11:51:02 +0100

    completed code to check valid subscription, given that we already have subscription_id. Need to write code to get subscription id from oid or sub.

--- billing/check_subscription.py
+++ billing/check_subscription.py
@@ -10,7 +10,7 @@ AAD_grant_type = 'client_credentials'
 SaaS_api_version = '2018-08-31'
 
 
-def AAD_token():
+def get_AAD_token():
     #https://docs.microsoft.com/en-us/azure/marketplace/partner-center-portal/pc-AAD-registration#http-method
     url = f'https://login.microsoftonline.com/{AAD_tenantId}/oauth2/token'
 
@@ -24,24 +24,26 @@ def AAD_token():
     return r.json()['access_token']
 
 
-def check_subscription(subscription_id, token):
+def check_SaaS_subscription(subscription_id, token):
     #https://docs.microsoft.com/en-us/azure/marketplace/partner-center-portal/pc-saas-fulfillment-api-v2#get-subscription
     url =   f'https://marketplaceapi.microsoft.com/api/saas/subscriptions/' + \
-            f'{subscription_id}?api-version={SaaS_api_version}'
+            f'{subscription_id}'
 
-    r = requests.post(url, 
-        data={
-            'resource': AAD_resource, 
-            'client_id': AAD_client_id,
-            'client_secret': AAD_client_secret,
-            'grant_type': AAD_grant_type
-            })
+    r = requests.get(url, params = {'api-version': SaaS_api_version},
+                    headers = {'content-type': 'application/json',
+                               'authorization': f'Bearer {token}' })
 
-    return r.json()['saasSubscriptionStatus']
+    status = r.json()['saasSubscriptionStatus']
+    assert status == 'Subscribed', '401 User not subscribed'
 
 
 if __name__ == '__main__':
-    token = AAD_token()
-    # print(token)
+    subscribed_subscription_id = '6b71cfa4-fa75-3540-fc41-b72fd8ef2555'
+    unsubscribed_subscription_id = 'e51bc4ed-f54e-36a7-8974-1fb9b52da316'
+    
+    token = get_AAD_token()
 
-    status = check_subscription()
+    status = check_SaaS_subscription(subscribed_subscription_id, token)
+#    status = check_SaaS_subscription(unsubscribed_subscription_id, token)
+
+
